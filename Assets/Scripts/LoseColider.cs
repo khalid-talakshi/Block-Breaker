c87a1946f02b1db2235ecd1498fdc057d10e18c4
coolk19 <khalid.talakshi@outlook.com>
2016-11-18 18:59:38 -0500

    Game Update: Back on Track
    Created a LoseColider that will trigger a scene change from main game to
    win screen. Cleaned up code and project and added ball asset.
    
    Lecture Currently On: Choosing Collider Type in Unity (Section 5, Lesson
    15/72)

---
+++ Assets/Scripts/LoseColider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseColider : MonoBehaviour {
+
+public LevelManager levelmanager;
+
+	void OnTriggerEnter2D (Collider2D trigger) {
+		
+		print("Trigger");
+		levelmanager.LoadLevel("Win Screen");
+		
+		
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+	
+		print("Collision");
+		
+	}
+}
