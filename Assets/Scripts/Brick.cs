d874b433c331957b84732876662b07c395e97660
coolk19 <khalid.talakshi@outlook.com>
2016-12-02 21:08:21 -0500

    tried to change color

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -20,21 +20,37 @@ public class Brick : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	
+	
+	
 	}
 	
 	void OnCollisionEnter2D (Collision2D collision) {
 		
-		timesHit ++;
-		if (timesHit >= maxHits) {
+		maxHits --;
 		
+		if (maxHits == 3) {
+			
+			gameObject.renderer.material.color = new Color(0.604f, 0.349f, 0.710f); 
+			
+		} else if (maxHits == 2) {
+			
+			gameObject.renderer.material.color = new Color(0.180f, 0.796f, 0.439f);
+			
+		} else if (maxHits == 1) {
+			
+			gameObject.renderer.material.color = new Color(0.180f, 0.796f, 0.439f);
+			
+		} else if ( 0 >= maxHits) {
+			
 			Destroy(gameObject);
-		
+			
 		}
 		
 		
 	}
 	
-	//TODO Remove this method once we can actually win
+	//TODO Remove this method once we can actually win 0.180f, 0.796f, 0.439f
 	void SimulateWin() {
 		
 		levelmanager.loadNextLevel();
