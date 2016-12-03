c71d0fb272934418c2372b8b807bd79b7865340c
coolk19 <khalid.talakshi@outlook.com>
2016-12-02 21:24:32 -0500

    Reverted Changes as much as possible

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -2,44 +2,30 @@
 using System.Collections;
 
 public class Brick : MonoBehaviour {
-
+	
 	public int maxHits;
 	
 	private int timesHit;
+	private LevelManager levelmanager; 
 	
 	// Use this for initialization
 	void Start () {
 		
+		levelmanager = GameObject.FindObjectOfType<LevelManager>();
+		
 		timesHit = 0;
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	
-	
-	
+		
 	}
 	
 	void OnCollisionEnter2D (Collision2D collision) {
 		
-<<<<<<< HEAD
-		maxHits --;
-		
-		if (maxHits == 3) {
-			
-			gameObject.renderer.material.color = new Color(0.604f, 0.349f, 0.710f); 
-			
-		} else if (maxHits == 2) {
-			
-			gameObject.renderer.material.color = new Color(0.180f, 0.796f, 0.439f);
-			
-		} else if (maxHits == 1) {
-			
-			gameObject.renderer.material.color = new Color(0.180f, 0.796f, 0.439f);
-			
-		} else if ( 0 >= maxHits) {
+		timesHit ++;
+		if (timesHit >= maxHits) {
 			
 			Destroy(gameObject);
 			
@@ -48,14 +34,11 @@ public class Brick : MonoBehaviour {
 		
 	}
 	
-	//TODO Remove this method once we can actually win 0.180f, 0.796f, 0.439f
+	//TODO Remove this method once we can actually win
 	void SimulateWin() {
 		
 		levelmanager.loadNextLevel();
 		
-=======
-		timesHit ++;
->>>>>>> parent of df2b183... Added Destroy Mechanics
 		
 	}
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,15 +2,21 @@
 using System.Collections;
 
 public class LevelManager : MonoBehaviour {
-
+	
 	public void LoadLevel(string name){
 		Debug.Log ("New Level load: " + name);
 		Application.LoadLevel (name);
 	}
-
+	
 	public void QuitRequest(){
 		Debug.Log ("Quit requested");
 		Application.Quit ();
 	}
-
+	
+	public void loadNextLevel() {
+		
+		Application.LoadLevel(Application.loadedLevel + 1);
+		
+	}
+	
 }
